acaf78cc72733421951e5eb1c7b50e1d4201593b
Nicolas Sawczuk <nico.290698@hotmail.com>
2020-06-23 14:15:54 -0300

    Correccion en el sidebar (items open y active)
    Se agrego el control de liks en el sidebar para poder dejar abierto el item y el dropdown correspondiente segun la vista en la que se esté

--- resources/views/theme/admin-lte/sidebar.blade.php
+++ resources/views/theme/admin-lte/sidebar.blade.php
@@ -1,223 +1,235 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-light-primary elevation-4">
-    <!-- Brand Logo -->
-    <a class="brand-link">
-      <img src="" alt="" class="brand-image img-circle elevation-3"
-           style="opacity: .8">
-      <span class="brand-text font-weight-light h3">
-        @section('nombreGimnasio')
-          @isset($gimnasio)
-              <b>
-                @isset($gimnasio->reporte_configuracion)
-                <img src="{{asset("storage/".$gimnasio->reporte_configuracion->logo)}}" class="brand-image img-circle" style="opacity: .8">
-                @endisset
-                {{ $gimnasio->nombre }}
-              </b>
-          @endisset
-        @show
-      </span>
-    </a>
-  
-    <!-- Sidebar -->
-    <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <i class="fal fa-user-circle fa-2x"></i>
-        </div>
-        <div class="info">
-          <a href="#" class="d-block">
-            @auth
-            @section('usuario')
-            {{ Auth::user()->name }} {{ Auth::user()->apellido }}
-            @show
-            @endauth
-          </a>
-        </div>
+  <!-- Brand Logo -->
+  <a class="brand-link">
+    <img src="" alt="" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <span class="brand-text font-weight-light h3">
+      @section('nombreGimnasio')
+      @isset($gimnasio)
+      <b>
+        @isset($gimnasio->reporte_configuracion)
+        <img src="{{asset("storage/".$gimnasio->reporte_configuracion->logo)}}" class="brand-image img-circle"
+          style="opacity: .8">
+        @endisset
+        {{ $gimnasio->nombre }}
+      </b>
+      @endisset
+      @show
+    </span>
+  </a>
+
+  <!-- Sidebar -->
+  <div class="sidebar">
+    <!-- Sidebar user panel (optional) -->
+    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      <div class="image">
+        <i class="fal fa-user-circle fa-2x"></i>
       </div>
-  
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          @section('sidebarNenu')
-          <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fal fa-dumbbell"></i>
-              <p>
-                 Gimnasio
-                <i class="fas fa-angle-left right"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="/gimnasios/administrar" class="nav-link">
-                  <i class="fal fa-circle nav-icon"></i>
-                  <p>Administración</p>
-                </a>
-              </li>
-              @isset($gimnasio)
-              <li class="nav-item">
-                <a href="/clientes/administrar/{{ $gimnasio->id }}" class="nav-link">
-                  <i class="fal fa-circle nav-icon"></i>
-                  <p>
-                    Clientes
-                    <span class="badge badge-secondary right">{{ $gimnasio->getClientes() }}</span>
-                  </p>
-                </a>
-              </li>
-              <li class="nav-item">
-              <a href="{{route('gimnasios.estadistica', [$gimnasio->id, $gimnasio->slug()])}}" class="nav-link">
-                  <i class="fal fa-circle nav-icon"></i>
-                  <p>Estadística</p>
-                </a>
-              </li>
-              @endisset
-            </ul>
-          </li>
-          <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fad fa-stream"></i>
-              <p>
-                Especialidades
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              @isset($gimnasio)
-              <li class="nav-item">
-                <a href="{{route('especialidades.administrar',[$gimnasio->id, $gimnasio->slug()])}}" class="nav-link">
-                  <i class="fal fa-circle nav-icon"></i>
-                  <p>Administración</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="{{route('especialidades.administrarMisEspecialidades',[$gimnasio->id, $gimnasio->slug()])}}" class="nav-link">
-                  <i class="fal fa-circle nav-icon"></i>
-                  <p>Mis especialidades</p>
-                </a>
-              </li>
-              @endisset
-              @isset($gimnasio)
-              <li class="nav-item">
-                <a href="{{route('especialidades.estadistica', [$gimnasio->id, $gimnasio->slug()])}}" class="nav-link">
-                  <i class="fal fa-circle nav-icon"></i>
-                  <p>Estadística</p>
-                </a>
-              </li>
-              @endisset
-            </ul>
-          </li>
-          <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fal fa-cash-register"></i>
-              <p>
-                 Cuotas
-                <i class="fas fa-angle-left right"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              @isset($gimnasio)
-              <li class="nav-item">
-                <a href="{{route('cuotas.administrar', [$gimnasio->id, $gimnasio->slug()])}}" class="nav-link">
-                  <i class="fal fa-circle nav-icon"></i>
-                  <p>
-                    Administración
-                  </p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="{{route('cuotas.administrarMisCuotas', [$gimnasio->id, $gimnasio->slug()])}}" class="nav-link">
-                  <i class="fal fa-circle nav-icon"></i>
-                  <p>Mis cuotas</p>
-                </a>
-              </li>
-              @endisset
-            </ul>
-          </li>
-          <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fad fa-cog"></i>
-              <p>
-                Configuración
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              @isset($gimnasio)
-              <li class="nav-item">
-                <a href="/email_configuracion/{{ $gimnasio->id }}/edit" class="nav-link">
-                  <i class="fal fa-circle nav-icon"></i>
-                  <p>Correos automáticos</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="/reporte_configuracion/{{ $gimnasio->id }}/edit" class="nav-link">
-                  <i class="fal fa-circle nav-icon"></i>
-                  <p>Reportes</p>
-                </a>
-              </li>
-              @endisset
-            </ul>
-          </li>
-          @can('users.index')
-          @isset($gimnasio)
-          <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fad fa-tools"></i>
-              <p>
-                Panel de administrador
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="/usuarios/administrar/{{$gimnasio->id}}" class="nav-link">
-                  <i class="fal fa-users-cog nav-icon"></i>
-                  <p>Usuarios, roles y permisos</p>
-                </a>
-              </li>
-            </ul>
-          </li>   
-          @endisset
-          @endcan
-          <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fad fa-door-open"></i>
-              <p>
-                Mi sesión
-                <i class="fas fa-angle-left right"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              @if (session('status'))
-              <li class="nav-item">
-                <a href="pages/examples/login.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Login</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/examples/register.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Register</p>
-                </a>
-              </li>
-              @endif
-              <li class="nav-item">
-                <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="nav-link">
-                  <i class="fas fa-sign-out nav-icon"></i>
-                  <p>Cerrar sesión</p>
-                </a>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                  {{ csrf_field() }}
-                </form>
-              </li>
-            </ul>
-          </li>
+      <div class="info">
+        <a href="#" class="d-block">
+          @auth
+          @section('usuario')
+          {{ Auth::user()->name }} {{ Auth::user()->apellido }}
           @show
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
+          @endauth
+        </a>
+      </div>
     </div>
-    <!-- /.sidebar -->
-  </aside>
+
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        @section('sidebarNenu')
+        <li
+          class="nav-item has-treeview {{ (request()->routeIs('gimnasios.*')) ? 'menu-open' : '' }} {{ (request()->routeIs('clientes.*')) ? 'menu-open' : '' }}">
+          <a href="#" class="nav-link">
+            <i class="nav-icon fal fa-dumbbell"></i>
+            <p>
+              Gimnasio
+              <i class="fas fa-angle-left right"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="/gimnasios/administrar" class="nav-link">
+                <i class="fal fa-circle nav-icon"></i>
+                <p>Administración</p>
+              </a>
+            </li>
+            @isset($gimnasio)
+            <li class="nav-item">
+              <a href="{{route('clientes.administrar',$gimnasio->id)}}"
+                class="nav-link {{ (request()->routeIs('clientes.*')) ? 'active' : '' }}">
+                <i class="fal fa-circle nav-icon"></i>
+                <p>
+                  Clientes
+                  <span class="badge badge-secondary right">{{ $gimnasio->getClientes() }}</span>
+                </p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="{{route('gimnasios.estadistica', [$gimnasio->id, $gimnasio->slug()])}}"
+                class="nav-link {{ (request()->routeIs('gimnasios.*')) ? 'active' : '' }}">
+                <i class="fal fa-circle nav-icon"></i>
+                <p>Estadística</p>
+              </a>
+            </li>
+            @endisset
+          </ul>
+        </li>
+        <li class="nav-item has-treeview {{ (request()->routeIs('especialidades.*')) ? 'menu-open' : '' }}">
+          <a href="#" class="nav-link">
+            <i class="nav-icon fad fa-stream"></i>
+            <p>
+              Especialidades
+              <i class="right fas fa-angle-left"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            @isset($gimnasio)
+            <li class="nav-item">
+              <a href="{{route('especialidades.administrar',[$gimnasio->id, $gimnasio->slug()])}}"
+                class="nav-link  {{ (request()->routeIs('especialidades.administrar')) ? 'active' : '' }}">
+                <i class="fal fa-circle nav-icon"></i>
+                <p>Administración</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="{{route('especialidades.administrarMisEspecialidades',[$gimnasio->id, $gimnasio->slug()])}}"
+                class="nav-link  {{ (request()->routeIs('especialidades.administrarMisEspecialidades')) ? 'active' : '' }}">
+                <i class="fal fa-circle nav-icon"></i>
+                <p>Mis especialidades</p>
+              </a>
+            </li>
+            @endisset
+            @isset($gimnasio)
+            <li class="nav-item">
+              <a href="{{route('especialidades.estadistica', [$gimnasio->id, $gimnasio->slug()])}}"
+                class="nav-link  {{ (request()->routeIs('especialidades.estadistica')) ? 'active' : '' }}">
+                <i class="fal fa-circle nav-icon"></i>
+                <p>Estadística</p>
+              </a>
+            </li>
+            @endisset
+          </ul>
+        </li>
+        <li class="nav-item has-treeview {{ (request()->routeIs('cuotas.*')) ? 'menu-open' : '' }}">
+          <a href="#" class="nav-link">
+            <i class="nav-icon fal fa-cash-register"></i>
+            <p>
+              Cuotas
+              <i class="fas fa-angle-left right"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            @isset($gimnasio)
+            <li class="nav-item">
+              <a href="{{route('cuotas.administrar', [$gimnasio->id, $gimnasio->slug()])}}"
+                class="nav-link {{ (request()->routeIs('cuotas.administrar')) ? 'active' : '' }}">
+                <i class="fal fa-circle nav-icon"></i>
+                <p>
+                  Administración
+                </p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="{{route('cuotas.administrarMisCuotas', [$gimnasio->id, $gimnasio->slug()])}}"
+                class="nav-link {{ (request()->routeIs('cuotas.administrarMisCuotas')) ? 'active' : '' }}">
+                <i class="fal fa-circle nav-icon"></i>
+                <p>Mis cuotas</p>
+              </a>
+            </li>
+            @endisset
+          </ul>
+        </li>
+        <li
+          class="nav-item has-treeview {{ (request()->routeIs('email_configuracion.*')) ? 'menu-open' : '' }} {{ (request()->routeIs('reporte_configuracion.*')) ? 'menu-open' : '' }}">
+          <a href="#" class="nav-link">
+            <i class="nav-icon fad fa-cog"></i>
+            <p>
+              Configuración
+              <i class="right fas fa-angle-left"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            @isset($gimnasio)
+            <li class="nav-item">
+              <a href="/email_configuracion/{{ $gimnasio->id }}/edit"
+                class="nav-link {{ (request()->routeIs('email_configuracion.edit')) ? 'active' : '' }}">
+                <i class="fal fa-circle nav-icon"></i>
+                <p>Correos automáticos</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="/reporte_configuracion/{{ $gimnasio->id }}/edit"
+                class="nav-link {{ (request()->routeIs('reporte_configuracion.edit')) ? 'active' : '' }}">
+                <i class="fal fa-circle nav-icon"></i>
+                <p>Reportes</p>
+              </a>
+            </li>
+            @endisset
+          </ul>
+        </li>
+        @can('users.index')
+        @isset($gimnasio)
+        <li class="nav-item has-treeview {{ (request()->routeIs('usuarios.*')) ? 'menu-open' : '' }}">
+          <a href="#" class="nav-link">
+            <i class="nav-icon fad fa-tools"></i>
+            <p>
+              Panel de administrador
+              <i class="right fas fa-angle-left"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="/usuarios/administrar/{{$gimnasio->id}}" class="nav-link {{ (request()->routeIs('usuarios.administrar')) ? 'active' : '' }}">
+                <i class="fal fa-users-cog nav-icon"></i>
+                <p>Usuarios, roles y permisos</p>
+              </a>
+            </li>
+          </ul>
+        </li>
+        @endisset
+        @endcan
+        <li class="nav-item has-treeview">
+          <a href="#" class="nav-link">
+            <i class="nav-icon fad fa-door-open"></i>
+            <p>
+              Mi sesión
+              <i class="fas fa-angle-left right"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            @if (session('status'))
+            <li class="nav-item">
+              <a href="pages/examples/login.html" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Login</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="pages/examples/register.html" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Register</p>
+              </a>
+            </li>
+            @endif
+            <li class="nav-item">
+              <a href="{{ route('logout') }}"
+                onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="nav-link">
+                <i class="fas fa-sign-out nav-icon"></i>
+                <p>Cerrar sesión</p>
+              </a>
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                {{ csrf_field() }}
+              </form>
+            </li>
+          </ul>
+        </li>
+        @show
+      </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+  </div>
+  <!-- /.sidebar -->
+</aside>
